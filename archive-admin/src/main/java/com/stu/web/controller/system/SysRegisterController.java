7ac2d45d9e9b884d4bda8088fa05200b4f85883f
zhu_jiahua <zhu_jiahua@internal.dev>
2024-10-22 19:37:12 +0800

    init project

---
+++ archive-admin/src/main/java/com/stu/web/controller/system/SysRegisterController.java
@@ -0,0 +1,38 @@
+package com.stu.web.controller.system;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.stu.common.core.controller.BaseController;
+import com.stu.common.core.domain.AjaxResult;
+import com.stu.common.core.domain.model.RegisterBody;
+import com.stu.common.utils.StringUtils;
+import com.stu.framework.web.service.SysRegisterService;
+import com.stu.system.service.ISysConfigService;
+
+/**
+ * 注册验证
+ * 
+ * @author ruoyi
+ */
+@RestController
+public class SysRegisterController extends BaseController
+{
+    @Autowired
+    private SysRegisterService registerService;
+
+    @Autowired
+    private ISysConfigService configService;
+
+    @PostMapping("/register")
+    public AjaxResult register(@RequestBody RegisterBody user)
+    {
+        if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser"))))
+        {
+            return error("当前系统没有开启注册功能！");
+        }
+        String msg = registerService.register(user);
+        return StringUtils.isEmpty(msg) ? success() : error(msg);
+    }
+}
